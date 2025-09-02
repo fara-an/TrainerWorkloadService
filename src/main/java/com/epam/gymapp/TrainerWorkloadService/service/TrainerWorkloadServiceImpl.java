2584e7af2cde3070142d16497cf26c93e9a88225
yodgora <anorayodgora@gmail.com>
2025-09-02 23:46:58 +0500

    removed unused imports

--- src/main/java/com/epam/gymapp/TrainerWorkloadService/service/TrainerWorkloadServiceImpl.java
+++ src/main/java/com/epam/gymapp/TrainerWorkloadService/service/TrainerWorkloadServiceImpl.java
@@ -8,7 +8,6 @@ import com.epam.gymapp.TrainerWorkloadService.model.YearSummary;
 import com.epam.gymapp.TrainerWorkloadService.repository.TrainerWorkloadRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
